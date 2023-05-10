8ba94a94dec7d17ae9a7c53ebc1e504a3f2d02d1
Валерий Иманов <imanov.03.valera@gmail.com>
2023-05-10 09:04:37 +0500

    Added repositories for Account and Chat

---
+++ DotMessenger.Core/Repositories/AccountRepository.cs
@@ -0,0 +1,46 @@
+﻿using DotMessenger.Core.Model.Entities;
+
+namespace DotMessenger.Core.Repositories
+{
+    public class AccountRepository : IRepository<Account>
+    {
+        public List<Account> Accounts { get; set; } = new List<Account>();
+
+        public void Add(Account entity)
+        {
+            if (entity == null) throw new ArgumentNullException("Unable to add empty account", nameof(entity));
+
+            Accounts.Add(entity);
+        }
+
+        public void Delete(Account entity)
+        {
+            if (entity == null) throw new ArgumentNullException("Unable to remove an empty chat", nameof(entity));
+            Accounts.Remove(entity);
+        }
+
+        public Account? FindById(int id)
+        {
+            return Accounts.SingleOrDefault(account => account.Id == id);
+        }
+
+        public IEnumerable<Account> GetAll()
+        {
+            foreach (var account in Accounts)
+            {
+                yield return account;
+            }
+        }
+
+        public void Update(Account entity)
+        {
+            if (entity == null) throw new ArgumentNullException("Unable to update to empty account", nameof(entity));
+
+            var foundAccount = FindById(entity.Id);
+
+            if (foundAccount == null) return;
+
+            foundAccount = entity;
+        }
+    }
+}
---
+++ DotMessenger.Core/Repositories/ChatRepository.cs
@@ -0,0 +1,44 @@
+﻿using DotMessenger.Core.Model.Entities;
+
+namespace DotMessenger.Core.Repositories
+{
+    public class ChatRepository : IRepository<Chat>
+    {
+        public List<Chat> Chats { get; set; } = new List<Chat>();
+        public void Add(Chat entity)
+        {
+            if (entity == null) throw new ArgumentNullException("Unable to add empty chat", nameof(entity));
+            Chats.Add(entity);
+        }
+
+        public void Delete(Chat entity)
+        {
+            if (entity == null) throw new ArgumentNullException("Unable to remove an empty chat", nameof(entity));
+            Chats.Remove(entity);
+        }
+
+        public Chat? FindById(int id)
+        {
+            return Chats.SingleOrDefault(account => account.Id == id);
+        }
+
+        public IEnumerable<Chat> GetAll()
+        {
+            foreach (var chat in Chats)
+            {
+                yield return chat;
+            }
+        }
+
+        public void Update(Chat entity)
+        {
+            if (entity == null) throw new ArgumentNullException("Unable to update to empty chat", nameof(entity));
+
+            var foundAccount = FindById(entity.Id);
+
+            if (foundAccount == null) return;
+
+            foundAccount = entity;
+        }
+    }
+}
